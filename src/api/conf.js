64f33f682a270ba6422d572d1e78c33adf536fb1
Alima Roman <59642740+matyevaa@users.noreply.github.com>
2022-05-25 21:31:59 -0700

    Update conf.js

--- src/api/conf.js
+++ src/api/conf.js
@@ -1,2 +1,2 @@
-export const API_KEY = '1cb9aeb785ddcade1eef5973db409596';
+export const API_KEY = '99ff89398473f9d980ffb71ca52dcbee';//'1cb9aeb785ddcade1eef5973db409596';
 export const API_SOURCE = 'http://api.openweathermap.org/';
